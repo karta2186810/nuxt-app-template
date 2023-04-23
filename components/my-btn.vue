ff6cf8da014922230c2b565174a193e6cd83d479
karta2186810 <karta2186810@gmail.com>
2023-04-23 17:56:55 +0800

    feat: add 404 error page

--- components/my-btn.vue
+++ components/my-btn.vue
@@ -1,8 +1,8 @@
 <template>
-  <button v-if="href !== null" class="btn" v-bind="$attrs">
+  <button v-if="href !== null" class="my-btn" v-bind="$attrs">
     <slot></slot>
   </button>
-  <a v-else class="btn" :href="href" v-bind="$attrs">
+  <a v-else class="my-btn" :href="href" v-bind="$attrs">
     <slot></slot>
   </a>
 </template>
@@ -15,7 +15,7 @@ defineProps({
 })
 </script>
 <style lang="scss" scoped>
-.btn {
+.my-btn {
   @apply inline-flex cursor-pointer select-none items-center whitespace-nowrap rounded-sm bg-gray-800 px-3 py-2 text-base text-white hover:bg-gray-900;
 }
 </style>
